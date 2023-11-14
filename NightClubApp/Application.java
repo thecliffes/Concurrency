00b778828877bc05914899837aaf66d233af2e15
Ben Cliffe <93997594+thecliffes@users.noreply.github.com>
2023-11-14 21:42:59 +0000

    Update Application.java

--- NightClubApp/Application.java
+++ NightClubApp/Application.java
@@ -1,10 +1,3 @@
-/**
- * You do not need to modify this class for submission, but may modify it 
- * to implement and test your solution.
- *
- * When marking your solution, we will use a new version of this file thus any
- * changes you make will not be marked.
-*/
 public class Application {
     public static void main(String[] args) throws InterruptedException {
         String name = "Fiction";
